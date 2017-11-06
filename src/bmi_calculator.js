f097777d42a2e03ad90c662d2342097e7c593fb0
aljoh <reachingalfred@gmail.com>
2017-11-06 15:24:35 +0100

    added specs for bmi calculator. implementation in progress

---
+++ src/bmi_calculator.js
@@ -0,0 +1,10 @@
+function BMICalculator(){
+};
+
+BMICalculator.prototype.metric_bmi = function(obj) {
+  var weight = obj.weight;
+  var height = obj.height;
+  if (weight > 0 && height > 0) {
+    var finalBmi = weight / (height / 100 * height / 100);
+  }
+};
