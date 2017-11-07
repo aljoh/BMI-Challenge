4ed330d538adfe0186b43e798c997f3dc19345a2
aljoh <reachingalfred@gmail.com>
2017-11-07 11:20:07 +0100

    added first spec for imperial

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,25 +1,39 @@
 describe("Person", function() {
   var person;
 
-  beforeEach(function() {
-    person = new Person({weight: 90, height: 186});
+  describe("Can calculate metric bmi", function() {
+
+    beforeEach(function() {
+      person = new Person({weight: 90, height: 186});
+    });
+
+    it("should have weight of 90", function() {
+      expect(person.weight).toEqual(90);
+    });
+
+    it("should have height of 186", function() {
+      expect(person.height).toEqual(186);
+    });
+
+    it("should calculate BMI value", function() {
+      person.calculate_bmi();
+      expect(person.bmiValue).toEqual(26.01);
+    });
+
+    it("should have a BMI message", function() {
+      person.calculate_bmi();
+      expect(person.bmiMessage).toEqual("Overweight");
+    });
   });
 
-  it("should have weight of 90", function() {
-    expect(person.weight).toEqual(90);
-  });
+  describe("Can calculate imperial bmi", function() {
 
-  it("should have height of 186", function() {
-    expect(person.height).toEqual(186);
-  });
+    beforeEach(function() {
+      person = new Person({weight: 210, height: 74});
+    });
 
-  it("should calculate BMI value", function() {
-    person.calculate_bmi();
-    expect(person.bmiValue).toEqual(26.01);
-  });
-
-  it("should have a BMI message", function() {
-    person.calculate_bmi();
-    expect(person.bmiMessage).toEqual("Overweight");
+    it("should have a weight of 210", function() {
+      expect(person.weight).toEqual(210);
+    });
   });
 });
