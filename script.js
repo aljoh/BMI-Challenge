e4723c2afa389c7684e127eb06d0074399d67d7f
aljoh <reachingalfred@gmail.com>
2017-11-08 14:11:13 +0100

    moved js from html to own file

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#calculate').click(function() {
+    var w = parseFloat($('#weight').val());
+    var h = parseFloat($('#height').val());
+    var person = new Person({
+      weight: w,
+      height: h
+    });
+    if ($('#imperial').is(":checked")) {
+      person.calculate_imperial_bmi();
+    } else {
+      person.calculate_bmi();
+    }
+    $('#display_value').html('Your BMI is ' + person.bmiValue);
+    $('#display_message').html('and you are ' + person.bmiMessage);
+  });
+});
