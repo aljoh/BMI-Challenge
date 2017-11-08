4c2c30e757f7517800c0950ee250d7372c95821e
aljoh <reachingalfred@gmail.com>
2017-11-08 16:00:13 +0100

    added functions for translation

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+  loadLocale = function(code) {
+    var locale = code || 'en';
+    $.ajax(`src/locales/${locale}.json`).done(function(text){
+      data = JSON.parse(text);
+      applyTranslation(data);
+    });
+  };
+  $('#english').addClass('disabled');
+  loadLocale();
   $('#calculate').click(function() {
     var w = parseFloat($('#weight').val());
     var h = parseFloat($('#height').val());
@@ -14,4 +23,26 @@ $(document).ready(function() {
     $('#display_value').html('Your BMI is ' + person.bmiValue);
     $('#display_message').html('and you are ' + person.bmiMessage);
   });
+
+  getLocaleCode = function(id) {
+    switch (id) {
+      case 'swedish':
+        loadLocale('sv');
+        break;
+      case 'english':
+        loadLocale('en');
+        break;
+    }
+  };
+
+  applyTranslation = function(data) {
+    i18n.translator.add(data);
+    $('h3').html(i18n('app_name'))
+    $('#calculate').html(i18n('calculate'));
+    $("#weight").attr("placeholder", i18n('weight'));
+    $("#height").attr("placeholder", i18n('height'));
+    $("#swedish").html(i18n('swedish'));
+    $("#english").html(i18n('english'));
+return;
+  };
 });
