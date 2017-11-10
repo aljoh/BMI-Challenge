36b455b960b9668676f5062e2cc5019fd7d04c63
aljoh <reachingalfred@gmail.com>
2017-11-10 21:13:23 +0100

    refactor removed unused code

--- src/person.js
+++ src/person.js
@@ -7,7 +7,3 @@ function Person(attr) {
 Person.prototype.calculate_bmi = function(system) {
   calculator.metric_bmi(this, system);
 };
-
-Person.prototype.calculate_imperial_bmi = function() {
-  calculator.imperial_bmi(this);
-};
