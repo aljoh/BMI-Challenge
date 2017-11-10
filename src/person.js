dfef73831d82d5f417e8d42e918525cb0a5b7b3e
aljoh <reachingalfred@gmail.com>
2017-11-10 19:54:18 +0100

    refactor

--- src/person.js
+++ src/person.js
@@ -1,14 +1,13 @@
 function Person(attr) {
   this.weight = attr.weight;
   this.height = attr.height;
+  calculator = new BMICalculator();
 }
 
 Person.prototype.calculate_bmi = function() {
-  calculator = new BMICalculator();
   calculator.metric_bmi(this);
 };
 
 Person.prototype.calculate_imperial_bmi = function() {
-  calculator = new BMICalculator();
   calculator.imperial_bmi(this);
 };
